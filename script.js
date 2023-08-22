b233144d8e41aee1079d53cd932ef6022bad546d
Ricardo <email>
2023-08-21 21:08:29 -0300

    Novas Alterações

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle("Light")
+  html.classList.toggle("light")
+
+  // pegar a tag img
   const img = document.querySelector("#profile img")
-  if (html.classList.contains("Light")) {
-    img.setAttribute("src", "/Assets/avatar-light.png")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "/Assets/Avatar.png")
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
-
-//pegar a tag image
-// substituir a imagem
-//se tiver light mode adicionar a imagem, adicionar a imagem light
-// se tiver sem light mode, manter a imagem normal
